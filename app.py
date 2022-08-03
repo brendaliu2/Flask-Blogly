e8b7a93cb165f9f72b3939dedca2a51c070b3438
Margaret Sun <hi.margaret.sun@gmail.com>
2022-08-03 16:46:58 -0700

    add doctest

--- app.py
+++ app.py
@@ -18,19 +18,23 @@ db.create_all()
 
 @app.get('/')
 def redirect_to_users():
+    """Redirect to a list of users"""
     return redirect('/users')
 
 @app.get('/users')
 def show_all_users():
+    """Displays list of users page"""
     users = User.query.all()
     return render_template('user.html', users = users)
 
 @app.get('/users/new')
 def display_new_user_form():
+    """Displays new user form"""
     return render_template('new_user.html')
 
 @app.post('/users/new')
 def add_new_user():
+    """Adds new users and redirect to list of users page"""
 
     first_name = request.form['first-name']
     last_name = request.form['last-name']
@@ -48,6 +52,7 @@ def add_new_user():
 
 @app.get('/users/<user_id>')
 def display_user(user_id):
+    """Displays current user"""
 
     user = User.query.get_or_404(user_id)
 
@@ -56,11 +61,13 @@ def display_user(user_id):
 
 @app.get('/users/<user_id>/edit')
 def display_edit_form(user_id):
+    """Displays edit form for current user"""
     user = User.query.get_or_404(user_id)
     return render_template('edit_user.html', user = user)
 
 @app.post('/users/<user_id>/edit')
 def edit_user(user_id):
+    """Makes changes to current user's data"""
 
     user = User.query.get_or_404(user_id)
 
@@ -82,6 +89,7 @@ def edit_user(user_id):
 
 @app.post('/users/<user_id>/delete')
 def delete_user(user_id):
+    """Archives user and redirects to list of users page"""
 
     user = User.query.get_or_404(user_id)
     user.archived = True
